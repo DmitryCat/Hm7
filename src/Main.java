e1bebaab5d3c33faa429448f38d51202ab6d0d42
DimaCat <dimiiiitriy@gmail.com>
2023-11-26 04:03:41 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1(new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7)));
+        task2(new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7)));
+        task3(new ArrayList<>(List.of("dog", "cat", "fish", "fish")));
+        task4(new ArrayList<>(List.of("один", "два", "два", "три", "три", "три")));
+    }
+
+    public static void task1(List<Integer> nums) {
+        for (Integer num : nums) {
+            if (num % 2 > 0) {
+                System.out.print(num + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void task2(List<Integer> nums) {
+        Set<Integer> uniqueNums = new HashSet<>();
+        for (Integer num : nums) {
+            if (num % 2 == 0 && !uniqueNums.contains(num)) {
+                System.out.print(num + " ");
+                uniqueNums.add(num);
+            }
+        }
+        System.out.println();
+    }
+        public static void task3 (List<String> strings) {
+                Set < String > uniqueWords = new HashSet<>(strings);
+        System.out.println(uniqueWords);
+    }
+    public static void task4(List<String> strings) {
+        Map<String, Integer> wordsMap = new HashMap<>();
+        for (String string : strings) {
+            if (wordsMap.containsKey(string)) {
+                Integer currentRepearts = wordsMap.get(string);
+                wordsMap.put(string, currentRepearts + 1);
+            } else {
+                wordsMap.put(string, 1);
+            }
+            System.out.println(wordsMap);
+        }
     }
 }
